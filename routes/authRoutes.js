eb8bc97f18510eeaf51814bb84348c9b103ec83e
mallikarjunabakka <mallikarjunabakka99@gmail.com>
2026-04-20 23:04:58 +0530

    Updated backend auth fixes

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -81,7 +81,12 @@ router.post('/login', async (req, res) => {
 
     res.json({
       message: "Login successful",
-      token
+      token,
+    user: {
+    id: user._id,
+    name: user.name,
+    email: user.email
+  }
     });
 
   } catch (error) {
